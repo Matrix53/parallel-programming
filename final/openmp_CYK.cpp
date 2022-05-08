1532b265ac481f7541a8cd514658b2a9167cf64e
Qin Zhou <1079207272@qq.com>
2022-05-08 22:49:13 +0800

    chore: some update

--- final/openmp_CYK.cpp
+++ final/openmp_CYK.cpp
@@ -13,6 +13,7 @@ const int MAX_VN = 128;
 const int MAX_VT = 128;
 const int MAX_SLEN = 1024;
 const int MAX_SINGLE_PRODUNCTION2 = 24;
+const int MAX_VN_SMALL = 10;
 
 typedef struct Production1 {
   int parent;
@@ -32,8 +33,11 @@ int vn_num, p2_num, p1_num, slen;
 Production2 p2[MAX_PRODUCTION2_NUM];
 Production1 p1[MAX_PRODUCTION1_NUM];
 char str[MAX_SLEN];
-int dp[MAX_SLEN][MAX_SLEN][MAX_VN];  // 区间 [i, j] 由非终结符到情况总数的映射
-int p22[MAX_VN][MAX_SINGLE_PRODUNCTION2];                 // 整理Production2，提高空间局部性，用数组手写vector
+
+// 区间 [i, j] 由非终结符到情况总数的映射
+int dp[MAX_SLEN][MAX_SLEN][MAX_VN];
+// 整理Production2，提高空间局部性，用数组手写vector
+int p22[MAX_VN][MAX_SINGLE_PRODUNCTION2];
 
 // 解决旧版本GCC的OpenMP和optimize编译选项冲突的问题，详情见https://gcc.gnu.org/bugzilla/show_bug.cgi?id=82374
 void omp_for_1(int index) {
@@ -102,6 +106,34 @@ void omp_for_4(int len, int start) {
   }
 }
 
+// s代表small，适用于vn_num较小的情况
+// 区间 [i, j] 由非终结符到情况总数的映射
+int dps[MAX_SLEN][MAX_SLEN][MAX_VN_SMALL];
+// 整理Production2，提高空间局部性，用数组手写vector
+int p22s[MAX_VN_SMALL][MAX_SINGLE_PRODUNCTION2];
+
+void omp_for_5(int index) {
+  // 枚举所有终结产生式
+  for (int k = 0; k < p1_num; ++k) {
+    if (str[index] == p1[k].child) {
+      dps[index][index][p1[k].parent] = 1;
+    }
+  }
+}
+void omp_for_6(int len, int start) {
+  int end = start + len - 1;
+  for (int mid = start; mid < end; ++mid) {
+    // 枚举所有非终结产生式
+    for (int i = 0; i < vn_num; ++i) {
+      int sum = 0;
+      for (int j = 1; j < p22s[i][0]; j += 2) {
+        sum += dps[start][mid][p22s[i][j]] * dps[mid + 1][end][p22s[i][j + 1]];
+      }
+      dps[start][end][i] += sum;
+    }
+  }
+}
+
 int main() {
   freopen("input.txt", "r", stdin);
   scanf("%d\n", &vn_num);
@@ -117,7 +149,30 @@ int main() {
   // 计时开始
   steady_clock::time_point t1 = steady_clock::now();
 
-  if (p2_num < 300 || slen < 500) {
+  if (vn_num <= 10) {
+    // 重新整理Production2
+    for (int i = 0; i < p2_num; ++i) {
+      p22s[p2[i].parent][++p22s[p2[i].parent][0]] = p2[i].child1;
+      p22s[p2[i].parent][++p22s[p2[i].parent][0]] = p2[i].child2;
+    }
+
+    // dp之前的必要初始化
+    #pragma omp parallel for schedule(dynamic, 8)
+    for (int i = 0; i < slen; ++i) {
+      omp_for_5(i);
+    }
+
+    // 区间 dp
+    for (int len = 2; len <= slen; ++len) {
+      #pragma omp parallel for schedule(dynamic)
+      for (int start = 0; start <= slen - len; ++start) {
+        omp_for_6(len, start);
+      }
+    }
+
+    // 输出结果
+    printf("%u\n", dps[0][slen - 1][0]);
+  } else if (slen < 500) {
     // 重新整理Production2
     for (int i = 0; i < p2_num; ++i) {
       p22[p2[i].parent][++p22[p2[i].parent][0]] = p2[i].child1;
@@ -137,6 +192,9 @@ int main() {
         omp_for_2(len, start);
       }
     }
+
+    // 输出结果
+    printf("%u\n", dp[0][slen - 1][0]);
   } else {
     // TODO 并行优化，对Production2进行排序
     sort(p2, p2 + p2_num, [](const Production2& a, const Production2& b) {
@@ -167,13 +225,14 @@ int main() {
         omp_for_4(len, start);
       }
     }
+
+    // 输出结果
+    printf("%u\n", dp[0][slen - 1][0]);
   }
 
   // 计时结束
   steady_clock::time_point t2 = steady_clock::now();
   duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
   printf("Total Time: %fs\n", time_span.count());
-
-  printf("%u\n", dp[0][slen - 1][0]);
   return 0;
 }
